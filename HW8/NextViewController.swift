57ccf34c02912711683d857a312709a62eac41c2
Khan <burov@ukr.net>
2020-04-26 18:35:40 +0300

    HM_8 refactoring of the third task

--- HW8/NextViewController.swift
+++ HW8/NextViewController.swift
@@ -13,84 +13,87 @@ class NextViewController: UIViewController {
     
     @IBOutlet weak var mainView: UIImageView!
     
+    var createdView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mainView.backgroundColor = UIColor.lightGray
         
-        
-        func createView(positionX: Int, positionY: Int, image: String, labelText: String) {
+        func createView(for index: Int) {
             
-            let createdView=UIView(frame: CGRect(x: positionX, y: positionY, width: 200, height: 300))
+            // Base x, y positioning and frame width and height
+            var currentX: CGFloat = 2
+            var currentY: CGFloat = 50
+            let width: CGFloat = 200
+            let height: CGFloat = 300
             
-            // Change UIView background colour
-            createdView.backgroundColor=UIColor.lightGray
+            // Main UIView background
+            mainView.backgroundColor = UIColor.lightGray
             
-            // Add border to UIView
-            createdView.layer.borderWidth = 1
-            
-            // Change UIView Border Color to Red
-            createdView.layer.borderColor = UIColor.red.cgColor
-            
-            // Add created UIView as a Subview of mainView
-            mainView.addSubview(createdView)
-            
-            // Add image from Assets
-            let viewImage = UIImage(named: "\(image)")
-            
-            // Add UIImage value to operate with image preferences
-            let selectedImage:UIImageView = UIImageView()
-            
-            // Setup image to scale to fill
-            selectedImage.contentMode = UIView.ContentMode.scaleToFill
-            
-            // Add image width and height
-            selectedImage.frame.size.width = 200
-            selectedImage.frame.size.height = 300
-            
-            // Display the selected image from Assets
-            selectedImage.image = viewImage
-            
-            // Add UIImage as a Subview of createdView
-            createdView.addSubview(selectedImage)
-            
-            // Add label with CGRect positionig
-            let label = UILabel(frame: CGRect(x: 50, y: 310, width: 100, height: 30))
-            
-            // Add label color, background color, label text align to center
-            label.textColor = UIColor.blue
-            label.backgroundColor = .brown
-            label.textAlignment = .center
-            
-            // Add label text
-            label.text = labelText
-            
-            // Add label as a Subview of createdView
-            createdView.addSubview(label)
-        }
-        
-        func createAllFourViews() {
-            
-            for i in 1...4 {
+            for i in 0...index-1 {
                 
-                if i == 1 {
-                    createView(positionX: 5, positionY: 50, image: "image1", labelText: "Test-01")
+                if i % 2 == 0 {
+                    createdView = UIView(frame: CGRect(x: currentX, y: currentY, width: width, height: height))
+                    currentX += createdView.frame.width + 10
+                }
+                if i % 2 == 1 {
+                    createdView = UIView(frame: CGRect(x: currentX, y: currentY, width: width, height: height))
+                    currentX += createdView.frame.width - 410
+                    currentY += createdView.frame.height + 80
                 }
                 
-                if i == 2 {
-                    createView(positionX: 210, positionY: 50, image: "image2", labelText: "Test-02")
-                }
+                let image = "image\(i)"
+                let labelText = "Label - \(i)"
                 
-                if i == 3 {
-                    createView(positionX: 5, positionY: 430, image: "image3", labelText: "Test-03")
-                }
+                // createdView = UIView(frame: CGRect(x: posX, y: posY, width: width, height: height))
                 
-                if i == 4 {
-                    createView(positionX: 210, positionY: 430, image: "image4", labelText: "Test-04")
-                }
+                //Change UIView background colour
+                createdView.backgroundColor=UIColor.lightGray
+                
+                // Add border to UIView
+                createdView.layer.borderWidth = 1
+                
+                // Change UIView Border Color to Red
+                createdView.layer.borderColor = UIColor.red.cgColor
+                
+                // Add created UIView as a Subview of mainView
+                mainView.addSubview(createdView)
+                
+                // Add image from Assets
+                let viewImage = UIImage(named: "\(image)")
+                
+                // Add UIImage value to operate with image preferences
+                let selectedImage:UIImageView = UIImageView()
+                
+                // Setup image to scale to fill
+                selectedImage.contentMode = UIView.ContentMode.scaleToFill
+                
+                // Add image width and height
+                selectedImage.frame.size.width = 200
+                selectedImage.frame.size.height = 300
+                
+                // Display the selected image from Assets
+                selectedImage.image = viewImage
+                
+                // Add UIImage as a Subview of createdView
+                createdView.addSubview(selectedImage)
+                
+                // Add label with CGRect positionig
+                let label = UILabel(frame: CGRect(x: 50, y: 310, width: 100, height: 30))
+                
+                // Add label color, background color, label text align to center
+                label.textColor = UIColor.blue
+                label.backgroundColor = .brown
+                label.textAlignment = .center
+                
+                // Add label text
+                label.text = labelText
+                
+                // Add label as a Subview of createdView
+                createdView.addSubview(label)
             }
         }
-        createAllFourViews()
+        createView(for: 4)
     }
 }
